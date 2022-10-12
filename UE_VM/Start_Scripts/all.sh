c495f1019812040c05652d86b7babcc5abfcf4ad
Lily <lilyhueg@gmail.com>
2022-10-12 06:11:44 +0200

    up ping amount in ping test

--- UE_VM/Start_Scripts/all.sh
+++ UE_VM/Start_Scripts/all.sh
@@ -4,4 +4,5 @@ sudo gnome-terminal --title="gnb" -- ./gnb.sh &
 echo "started gnb"
 sleep 3
 sudo  gnome-terminal --title="ue" -- ./start_all_ue.sh
-./Test_Scripts/test_all.sh
+sleep 10
+../Test_Scripts/test_all.sh
--- UE_VM/Test_Scripts/ping_test.sh
+++ UE_VM/Test_Scripts/ping_test.sh
@@ -5,7 +5,7 @@ URed='\033[0;31m'
 for var in "$@"
 do
 	packet_loss=100
-	packet_loss=$( ping -c 1 -I "$var" 8.8.8.8 | grep -oP '\d+(?=% packet loss)' )
+	packet_loss=$( ping -c 5 -I "$var" 8.8.8.8 | grep -oP '\d+(?=% packet loss)' )
 	#packet_loss=  $packet_loss | awk NF
 	#echo $packet_loss
 	if (( packet_loss == 0 ));
