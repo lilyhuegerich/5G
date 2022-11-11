bf014e02c7de1d88cbfa118e743f9c1949da3390
Lily <lilyhueg@gmail.com>
2022-11-12 00:08:13 +0100

    start ssh prep

--- UE_VM/Scan_Scripts/nmap_scan_core_host.sh
+++ UE_VM/Scan_Scripts/nmap_scan_core_host.sh
@@ -1,7 +1,7 @@
 purple='\033[0;35m'
 color_off='\033[0m'
 
-host=${1:- 10.45.0.1} # 5g core host address
+host=${1:- 10.0.2.4} # 5g core host address
 interface=${2:- uesimtun0}
 
 echo -e "${purple} Scanning host,  $host on interface $interface . "
@@ -11,7 +11,7 @@ echo -e "${color_off}"
 #nmap -v -Pn -e $interfave $host #if the hosts is bloking the pings try parralel dns
 
 #sudo nmap -p0- -A -T3 -e $interface $host # getting more aggresive
-#sudo nmap -p ssh -e $interface $host #check ssh
+sudo nmap -p ssh -e $interface $host #check ssh
 
 #sudo nmap -p 3868 -e $interface $host # check diameter
 sudo nmap -p 477 -e $interface $host #check ss7ns (udp)
--- UE_VM/Setup_Scripts/tools.sh
+++ UE_VM/Setup_Scripts/tools.sh
@@ -1,3 +1,11 @@
 sudo apt install nmap
 
 sudo apt install openssh-client
+
+sudo apt install hydra
+
+
+#optional
+sudo apt install vim
+
+
