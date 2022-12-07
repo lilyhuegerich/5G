e8a64e81e148ab999dd2d69eadd1089230699be1
Lily <lilyhueg@gmail.com>
2022-12-07 19:48:25 +0100

    adjust start

--- UE_VM/Start_Scripts/all.sh
+++ UE_VM/Start_Scripts/all.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
 cd /home/lily/5G/UE_VM/Start_Scripts
 sudo gnome-terminal --title="gnb" -- ./gnb.sh &
-#echo $! > gnb.pid
 echo "started gnb"
 sleep 6
 sudo  gnome-terminal --title="ue" -- ./start_all_ue.sh
 sleep 10
-wireshark &
+if (($#>0))
+then	
+	wireshark &
+fi
 ../Test_Scripts/test_all.sh
