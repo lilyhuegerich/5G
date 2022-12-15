3ddafac38c1d1a0cecd95a2ecf04390631d26ca3
Lily <lilyhueg@gmail.com>
2022-12-15 19:38:39 +0100

    gen all veths

---
+++ CORE_VM/Setup_Scripts/gen_NF_veths.sh
@@ -0,0 +1,45 @@
+./gen_veth_by_name.sh amf
+
+
+./gen_veth_by_name.sh ausf
+
+
+./gen_veth_by_name.sh bsf
+
+
+./gen_veth_by_name.sh hss
+
+
+./gen_veth_by_name.sh mme
+
+
+./gen_veth_by_name.sh nrf
+
+
+./gen_veth_by_name.sh nssf
+
+
+./gen_veth_by_name.sh pcf
+
+
+./gen_veth_by_name.sh pcrf
+
+
+./gen_veth_by_name.sh sgwc
+
+
+./gen_veth_by_name.sh sgwu
+
+
+./gen_veth_by_name.sh smf
+
+
+./gen_veth_by_name.sh udm
+
+
+./gen_veth_by_name.sh udr
+
+
+./gen_veth_by_name.sh upf
+
+
