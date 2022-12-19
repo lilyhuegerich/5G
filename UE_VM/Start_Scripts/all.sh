0447be7bb7e232e17c96c4d36b2cd3dc059ffc0b
Lily <lilyhueg@gmail.com>
2022-12-19 10:49:51 +0100

    fix merge problem

---
+++ UE_VM/Start_Scripts/all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo ip route add 10.0.3.0/24 dev enp0s3
+cd /home/lily/5G/UE_VM/Start_Scripts
+sudo gnome-terminal --title="gnb" -- ./gnb.sh &
+echo "started gnb"
+sleep 6
+sudo  gnome-terminal --title="ue" -- ./start_all_ue.sh
+sleep 10
+if [ -z "$1" ]
+then	
+	echo "wireshark &" TODO
+fi
+../Test_Scripts/test_all.sh
