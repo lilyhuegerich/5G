bb492b0ace8189c7f027f3a8d2e5073d3d831a6b
Lily <lilyhueg@gmail.com>
2022-12-20 12:13:17 +0100

    add path to upf

--- UE_VM/Start_Scripts/all.sh
+++ UE_VM/Start_Scripts/all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo ip route add 10.0.3.0/24 dev enp0s3
+sudo ip route add 10.0.3.0/24 dev enp0s3 #route to amf
+sudo ip route add 10.0.17.0/24 dev enp0s3 # route to upf
+
 cd /home/lily/5G/UE_VM/Start_Scripts
 sudo gnome-terminal --title="gnb" -- ./gnb.sh &
 echo "started gnb"
