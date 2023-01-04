ad137ebbd8805a39f18d7e657dbc02ca94980b0a
Lily <lilyhueg@gmail.com>
2023-01-04 16:05:27 +0100

    core pcaps, as well as nf naming

--- PCAPS/PCAP_readers/call_5g_visualizer.sh
+++ PCAPS/PCAP_readers/call_5g_visualizer.sh
@@ -1,4 +1,6 @@
 pcap_file={}
 
-python3 trace_visualizer.py -wireshark "OS" ../5G/PCAPS/GNB/start_add_ue0_ping.pcapng 
+#python3 trace_visualizer.py -wireshark "OS" ../5G/PCAPS/GNB/start_add_ue0_ping.pcapng 
+
+python3 trace_visualizer.py -wireshark "OS" -showheartbeat False -simple_diagrams True -openstackservers "../5G/PCAPS/PCAP_readers/iP_address_names.yaml" -force_order "UE_0,UE_1,UE_2,UE_3,gNB,UPF,AMF,SMF,SGWU,SGWC" -show_timestamp True ../5G/PCAPS/Core/full_start.pcapng 
 
