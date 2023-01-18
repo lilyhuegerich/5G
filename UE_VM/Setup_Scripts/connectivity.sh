5348120dea2feb508b4fdb861303c9beb0f0e0bf
Lily <lilyhueg@gmail.com>
2023-01-18 20:38:34 +0100

    configs for teo upf on one machine

--- UE_VM/Setup_Scripts/connectivity.sh
+++ UE_VM/Setup_Scripts/connectivity.sh
@@ -1,11 +1,10 @@
-sudo ip tuntap add name ogstun mode tun
-sudo ip addr add 10.45.0.1/16 dev ogstun
-sudo ip addr add 2001:db8:cafe::1/48 dev ogstun
-sudo ip link set ogstun up
+sudo ip tuntap add name ogstun2 mode tun
+sudo ip addr add 10.47.0.1/16 dev ogstun2
+sudo ip link set ogstun2 up
 
 
 sudo sysctl -w net.ipv6.conf.all.forwarding=1
-sudo iptables -t nat -A POSTROUTING -s 10.45.0.0/16
+sudo iptables -t nat -A POSTROUTING -s 10.47.0.0/16 ! -o ogstun2 -j MASQUERADE
 
 sudo ufw disable
 sudo ufw status
