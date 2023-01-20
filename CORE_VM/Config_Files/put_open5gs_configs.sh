3e27ae89fc6e00a6b16c7c5800c4a3451289b0e4
Lily <lilyhueg@gmail.com>
2023-01-20 14:34:16 +0100

    put_open5gs_configs.sh

--- CORE_VM/Config_Files/put_open5gs_configs.sh
+++ CORE_VM/Config_Files/put_open5gs_configs.sh
@@ -3,14 +3,14 @@ cd /home/lily/5G/CORE_VM/Config_Files #change me
 
 #sudo cp *.yaml /etc/open5gs
 
-src_dir="/home/lily/5G/CORE_VM/Config_File" #change me
+src_dir="/home/lily/5G/CORE_VM/Config_Files" #change me
 dst_dir="/etc/open5gs"
 
 for file in "$src_dir"/*.yaml; do
-	if [[ $file !=*"_"* ]]; then
+	if echo $file | grep -Eq '/[^_]*$'; then
 		sudo cp "$file" "$dst_dir"
 	fi
-
+done
 cp smf_2.yaml /home/lily/5G/SHARED/open5gs/install/etc/open5gs/smf.yaml
 
 cp upf_2.yaml /home/lily/5G/SHARED/open5gs/install/etc/open5gs/upf.yaml
