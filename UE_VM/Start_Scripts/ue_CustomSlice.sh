545dbf2032e712f54b9d667b987935b6895f8efc
Lily <lilyhueg@gmail.com>
2023-01-23 16:54:21 +0100

    start script adapted

--- UE_VM/Start_Scripts/ue_CustomSlice.sh
+++ UE_VM/Start_Scripts/ue_CustomSlice.sh
@@ -1,3 +1,3 @@
 cd /home/lily/UERANSIM  #RENAME ME
-sudo ./build/nr-ue -c /home/lily/5G/UE_VM/Config_Files/UE_files/ue_CustomSlice.yaml
+sudo ./build/nr-ue -c /home/lily/5G/UE_VM/Config_Files/"$1"/UE_files/ue_CustomSlice.yaml
 
