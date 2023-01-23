78357e06695fb9a80aa99e66e54e9748e9bbf905
Lily <lilyhueg@gmail.com>
2023-01-23 14:21:27 +0100

    classic core and adapted put configs

--- CORE_VM/Config_Files/put_open5gs_configs.sh
+++ CORE_VM/Config_Files/put_open5gs_configs.sh
@@ -1,6 +1,21 @@
+dir=${1:-"./"}
+cd $dir
+cwd=$(pwd)
+name=$(basename $cwd)
+echo "Setting Configs for " $name
 
-cd /home/lily/5G/CORE_VM/Config_Files #change me
+if [ -f put_configs.sh ]; then
+	./put_configs.sh #check if folder has its own put configs script 
+else
+	sudo cp *.yaml /etc/open5gs
+fi
 
-sudo cp *.yaml /etc/open5gs
+if [ -f change_state_off_all_open5gs_services.sh ]; then
+	./change_state_off_all_open5gs_services.sh
+else
+	cd /home/lily/5G/CORE_VM/Config_Files #change me
+	../Start_Scripts/change_state_off_all_open5gs_services.sh #restart all services
+fi
 
-../Start_Scripts/change_state_off_all_open5gs_services.sh #restart all services
+
+
