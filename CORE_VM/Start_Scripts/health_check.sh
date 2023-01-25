722aba600f7a12a1251487deccc16b3f979b0856
Lily <lilyhueg@gmail.com>
2023-01-25 14:54:00 +0100

    health check work in progress

--- CORE_VM/Start_Scripts/health_check.sh
+++ CORE_VM/Start_Scripts/health_check.sh
@@ -6,7 +6,7 @@ URed='\033[0;31m'
 standard_core_services=("mme" "sgwc" "smf" "amf" "sgwu" "upf" "hss" "pcrf" "nrf" "ausf" "udm" "pcf" "nssf" "bsf" "udr" )
 
 for service in "${standard_core_services[@]}"; do
-	if pgrep open5gs-$service > /dev/null ; then	
+	if pgrep -U open5gs open5gs-$service > /dev/null ; then	
 		echo -e "$UGreen" ""$service" is running."
 	else
 		echo -e "$URed" ""$service" is off."
