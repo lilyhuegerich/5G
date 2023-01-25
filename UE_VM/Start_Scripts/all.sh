82c37c92b27e4f7e3433e33c09dc1f46305a7db2
Lily <lilyhueg@gmail.com>
2023-01-25 17:16:39 +0100

    outsource file check

--- UE_VM/Start_Scripts/all.sh
+++ UE_VM/Start_Scripts/all.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
-config_files="/home/lily/5G/UE_VM/Config_Files"
-non_config_files=("GNB_files")
-default="Classic_Core"
+mode=${1:-"Classic_Core"}
+name=$(/home/lily/5G/SHARED/Tools/check_if_config_mode_valid.sh $mode UE_VM)
 
-if [ -d "$config_files/$1" ] && [ -n "$1" ]; then
-    echo "Starting Setup:" $1
-    name=$1
-else
-    echo $1 "Not a valid setup. Options are:"
-    for dir in "$config_files"/*/; do
-        config=true
-        for i in "${non_config_files[@]}"; do
-            if [[ "$i" == "$(basename $dir)" ]]; then
-                config=false
-            fi
-        done
-        if [ "$config" = true ]; then
-            echo "                           "$(basename "$dir")
-        fi
-    done
-    echo "Will use the default" $default
-    name=$default
-fi
-
+status=$?
+[ $status -eq 0 ] && echo "Mode: " $name || exit 1
+exit 0
 sudo ip route add 10.0.3.0/24 dev enp0s3 #route to amf
 sudo ip route add 10.0.17.0/24 dev enp0s3 # route to upf
 
