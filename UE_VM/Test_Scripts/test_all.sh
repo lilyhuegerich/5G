ab3c318cda295566556b9ac1a3969e36a564100e
Lily <lilyhueg@gmail.com>
2023-02-24 19:21:07 +0100

    fixed ue sending still messy

--- UE_VM/Test_Scripts/test_all.sh
+++ UE_VM/Test_Scripts/test_all.sh
@@ -1,2 +1,2 @@
 cd /home/lily/5G/UE_VM/Test_Scripts #RENAME ME
-./ping_test.sh -u uesimtun0 uesimtun1 uesimtun2 uesimtun3 -d 8.8.8.8
+./ping_test.sh -u uesimtun0 uesimtun1 uesimtun2 uesimtun3 -d 8.8.8.8 -a 5
