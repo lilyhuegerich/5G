ad9d4dd197529cc52901ef0cf1a0e04320b081d0
Lily <lilyhueg@gmail.com>
2023-04-04 11:22:30 +0200

    fix occasional ue start bug

--- UE_VM/Start_Scripts/start_all_ue.sh
+++ UE_VM/Start_Scripts/start_all_ue.sh
@@ -12,9 +12,13 @@ tmux split-window -h
 
 
 tmux send-keys -t 0 "./ue_internet.sh "$1"" Enter
+sleep .5
 tmux send-keys -t 1 "./ue_iot.sh "$1"" Enter
+sleep .5
 tmux send-keys -t 2 "./ue_edge.sh "$1"" Enter
+sleep .5
 tmux send-keys -t 3 "./ue_CustomSlice.sh "$1"" Enter
+sleep .5
 
 
 
